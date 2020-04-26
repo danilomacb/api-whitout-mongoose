edeaec396f28d9340ad9bf7a22dca9a9cdeb2cb6
danilomacb <danilo.bakun@gmail.com>
2020-04-26 08:15:04 -0300

    post

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 const express = require("express");
 const MongoClient = require("mongodb").MongoClient;
+const bodyParser = require("body-parser");
 
-const url = "mongodb://localhost:27017";
-const dbName = "myproject";
-
-const client = new MongoClient(url);
-
-client.connect((err) => {
+MongoClient("mongodb://localhost:27017").connect((err, client) => {
     if (err) return console.error("Error on connect to mongo: ", err);
 
     console.log("Connected correctly to mongo");
 
-    const db = client.db(dbName);
-    const collection = db.collection("documents");
+    const db = client.db("dbName");
+    const collection = db.collection("collectionName");
 
     const app = express();
-    const port = 3000;
+    app.use(bodyParser.json());
 
     app.get("/", async (req, res) => {
-        const documents = await collection.find().toArray();
-        res.send(documents);
+        const elements = await collection.find().toArray();
+        res.send(elements);
     });
 
-    app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+    app.post("/", async (req, res) => {
+        await collection.insert(req.body);
+        res.status(200).send();
+    });
+
+    app.listen(3000, () => console.log(`Example app listening at http://localhost:3000`));
 });
